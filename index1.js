45d216ea00627ef196c675b15b9eacff17105ea1
StarodubtsevN <starodubtsev_2004@bk.ru>
2024-07-04 12:41:29 +0300

    FIRST STEPS

---
+++ index1.js
@@ -0,0 +1,50 @@
+// //функция которая сравнивает два чилсла и слкладывает их, если первое 
+// //число больше второго и вычитает их, если это не так
+// var res = 1;
+// console.log('first count: ');
+// var num_1 = console.count();
+// console.log('second count: ');
+// var num_2 = console.count();
+
+//    function choice(res, num_1,num_2){
+        
+//         switch(res){
+//             case 1:
+//             var anser = 0; 
+//             anser =  num_1 + num_2;   
+            
+//             console.log('ответ: ' + anser);
+//             break
+//             case 0:
+//             anser = num_1 - num_2;
+            
+//             console.log('ответ: ' + anser);
+//             break;
+//         }
+//     }
+    
+//     if(num_1 > num_2){
+//         res = 0;
+//         choice(res,num_1,num_2);
+//     }else{
+//         res = 1;
+//         choice(res,num_1,num_2);
+//     }
+
+
+// console.log(Math.PI);
+// console.log(Math.min(1,2,3,4,5,6,7,8,9));
+// console.log(Math.max(1,2,3,4,10000,5,6,7,8,9));
+
+var arr = [5,true,'stroca',5.7,-1000];
+console.log(arr[0]);
+console.log(arr.length);
+var matrix = [
+[1,2,3],
+['wasd'],
+[true,false]
+];
+
+matrix [0][1] = 4;
+
+console.log(matrix);
